9e7c78967823cc1c14a87f8f872ba3ed767a1128
o.nikolaev <o.nikolaev@rtinform.ru>
2022-08-26 12:37:45 +0300

    Buttons and Events

--- main.py
+++ main.py
@@ -3,15 +3,33 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
 
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(200, 200, 300, 300)
+        self.setWindowTitle("Learn PyQt")
+        self.initUI()
+
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("My first label")
+        self.label.move(50, 50)
+
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Click me")
+        self.b1.move(150, 150)
+        self.b1.clicked.connect(self.clicked)
+
+    def clicked(self):
+        self.label.setText("you pressed the button")
+        self.update()
+
+    def update(self) -> None:
+        self.label.adjustSize()
+
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200, 200, 300, 300)
-    win.setWindowTitle("Learn PyQt 5")
-
-    label = QtWidgets.QLabel(win)
-    label.setText("My first label")
-    label.move(50, 50)
+    win = MyWindow()
 
     win.show()
     sys.exit(app.exec_())
